7a18206856e05c5b4133e60bd60b2a510e8869fa
a-int <somemailforicloud@icloud.com>
2023-08-23 11:47:55 +0300

    remove hello uri

--- main/server.c
+++ main/server.c
@@ -105,48 +105,6 @@ static esp_err_t upload_post_handler(httpd_req_t* req) {
   return ESP_OK;
 }
 
-static esp_err_t hello_get_handler(httpd_req_t *req)
-{
-    char*  buf;
-    size_t buf_len;
-
-    /* Get header value string length and allocate memory for length + 1,
-     * extra byte for null termination */
-    buf_len = httpd_req_get_hdr_value_len(req, "Host") + 1;
-    if (buf_len > 1) {
-        buf = malloc(buf_len);
-        /* Copy null terminated value string into buffer */
-        if (httpd_req_get_hdr_value_str(req, "Host", buf, buf_len) == ESP_OK) {
-            ESP_LOGI(TAG, "Found header => Host: %s", buf);
-        }
-        free(buf);
-    }
-
-    /* Set some custom headers */
-    httpd_resp_set_hdr(req, "Custom-Header-1", "Custom-Value-1");
-    httpd_resp_set_hdr(req, "Custom-Header-2", "Custom-Value-2");
-
-    /* Send response with custom headers and body set as the
-     * string passed in user context*/
-    const char* resp_str = (const char*) req->user_ctx;
-    httpd_resp_send(req, resp_str, HTTPD_RESP_USE_STRLEN);
-
-    /* After sending the HTTP response the old HTTP request
-     * headers are lost. Check if HTTP request headers can be read now. */
-    if (httpd_req_get_hdr_value_len(req, "Host") == 0) {
-        ESP_LOGI(TAG, "Request headers lost");
-    }
-    return ESP_OK;
-}
-
-static const httpd_uri_t hello = {
-    .uri       = "/hello",
-    .method    = HTTP_GET,
-    .handler   = hello_get_handler,
-    /* Let's pass response string in user
-     * context to demonstrate it's usage */
-    .user_ctx  = "Hello World!"
-};
 
 esp_err_t start_server(const char* base_path) {
   static struct file_server_data* server_data = NULL; //due to static only one instance is going to be even after few calls
@@ -168,7 +126,6 @@ esp_err_t start_server(const char* base_path) {
   server_config.uri_match_fn = httpd_uri_match_wildcard;
   if (httpd_start(&server, &server_config) == ESP_OK) {
     ESP_LOGI(TAG, "Registering URI...");
-    httpd_register_uri_handler(server, &hello);
     httpd_uri_t upload = {
       .uri = "/upload/*",
       .method = HTTP_POST,
